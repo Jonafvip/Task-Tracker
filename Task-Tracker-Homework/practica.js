cf958ba81161cdc49ba4615904e6d2d54a6a231e
Jona <zardax.david98@gmail.com>
2026-01-13 15:57:19 -0600

    Creacion de la logica Principal del CRUD

--- Task-Tracker-Homework/practica.js
+++ Task-Tracker-Homework/practica.js
@@ -1,1 +1,98 @@
+const fs = require("fs");
+const path = require("path");
+const File_Name = path.join(__dirname, "data.json");
 
+const argumentss = process.argv;
+const action = argumentss[2];
+
+const readTask = () => {
+    try {
+        const content = fs.readFileSync(File_Name, "utf-8");
+        return JSON.parse(content);
+    } catch {
+        return [];
+    }
+};
+
+const saveTask = (data) => {
+    const stringData = JSON.stringify(data, null, 2);
+    fs.writeFileSync(File_Name, stringData);
+};
+
+if (action === "add") {
+    const tasks = readTask();
+    const newTask = {
+        id: tasks.length > 0 ? Math.max(...tasks.map(t => t.id)) + 1 : 1,
+        description: argumentss[3],
+        status: "todo",
+        createdAt: new Date().toString(),
+        updatedAt: new Date().toString(),
+    };
+    const description = argumentss[3];
+
+    tasks.push(newTask);
+    saveTask(tasks);
+    console.log(`Task added successfully ${description}`);
+} else if (action === "list") {
+    const tasks = readTask();
+    const filter = argumentss[3];
+    console.log("----Task List----");
+    tasks.forEach((task) => {
+        if (!filter || task.status === filter) {
+            console.log(
+                `[ID: ${task.id} ${task.description} - Status: ${task.status}]`
+            );
+        }
+    });
+    console.log("Show all tasks...");
+} else if (action === "delete") {
+    const tasks = readTask();
+    const idDelete = Number(argumentss[3]);
+
+    const deleteTask = tasks.filter((task) => task.id !== idDelete);
+    if (tasks.length === deleteTask.length) {
+        console.log(`No se encontro ninguna tarea con el ID: ${idDelete}`);
+    } else {
+        saveTask(deleteTask);
+        console.log(`Task deleted successfully (ID:${idDelete})`);
+    }
+} else if (action === "update") {
+    const tasks = readTask();
+    const idUpdate = Number(argumentss[3]);
+    const newDescription = argumentss[4];
+
+    const updateTask = tasks.find((task) => task.id === idUpdate);
+
+    if (updateTask) {
+        updateTask.description = newDescription;
+        updateTask.updatedAt = new Date().toString();
+        saveTask(tasks);
+        console.log(`Task ${idUpdate} updated successfully to: ${newDescription}`);
+    } else {
+        console.log("Task not found");
+    }
+} else if (action === "mark-in-progress") {
+    const tasks = readTask();
+    const idUpdate = Number(argumentss[3]);
+    const taskToUpdate = tasks.find((t) => t.id === idUpdate);
+
+    if (taskToUpdate) {
+        taskToUpdate.status = "in-progress";
+        taskToUpdate.updatedAt = new Date().toString();
+        saveTask(tasks);
+        console.log(`task ${idUpdate} marked as in progress`);
+    }
+} else if (action === "mark-done") {
+    const tasks = readTask();
+    const idUpdate = Number(argumentss[3]);
+    const taskToUpdate = tasks.find((t) => t.id === idUpdate);
+
+    if (taskToUpdate) {
+        taskToUpdate.status = "done";
+        taskToUpdate.updatedAt = new Date().toString();
+        saveTask(tasks);
+        console.log(`task ${idUpdate} marked as in progress`);
+    }
+} else {
+    console.log("unrecognized command...");
+}
